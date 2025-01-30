720281a22e878b11eeb5230b983a6c3d8ef896ec
GabrielaAlcaraz <gabriela.alcaraz@cetys.mx>
2025-01-29 16:12:42 -0800

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Gabriela Sanchez on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
